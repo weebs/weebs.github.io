81c40ebc8531137bb1f501b344f56b85cbaaee1a
dave <147543+weebs@users.noreply.github.com>
2024-02-05 23:07:00 -0600

    update demo

--- web.js
+++ web.js
@@ -16,13 +16,11 @@
 
 const LAYOUT_CONFIG_KEY = 'layoutConfig';
 
-const initialProgram =
-`
+const initialProgram = `
 let code () =
     let items = System.Collections.Generic.List<int>()
     for i in 1..10 do
         items.Add(i * 2)
-    items.Add(11)
     printfn "hello"
     for i in 0..items.Count - 1 do
         printfn $"items[{i}] = {items[i]}"
